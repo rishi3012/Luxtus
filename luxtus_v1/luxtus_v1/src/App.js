ddd8d3be6f03c16ad15e0c56ff94ddd239628fd7
Rishi <rishipatel3012@gmail.com>
2023-12-02 23:45:39 +1100

    added files using npm install

---
+++ luxtus_v1/luxtus_v1/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Announcement from './components/announcementBar';
+
+function App() {
+  return (
+    <Announcement />
+  );
+}
+
+export default App;
