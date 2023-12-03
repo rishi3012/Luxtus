f0c1d51a95446956a518d77d4f7e4857c8c48f77
Rishi Patel <rishipatel3012@gmail.com>
2023-12-04 00:45:17 +1100

    packages and fonts updated

--- luxtus_v1/luxtus_v1/src/App.js
+++ luxtus_v1/luxtus_v1/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Announcement from './components/announcementBar';
+import Menubar from './components/Menu.jsx';
 
 function App() {
   return (
+    <>
     <Announcement />
+    <Menubar />
+    </>
   );
 }
 
