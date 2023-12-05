dd3d559905083ee1af2a42c049e8e7e0214a41b2
Rishi <rishipatel3012@gmail.com>
2023-12-05 15:59:39 +1100

    initial layout of the banner and added the banner to App js

--- luxtus_v1/luxtus_v1/src/App.js
+++ luxtus_v1/luxtus_v1/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Announcement from './components/announcementBar';
 import Menubar from './components/Menu.jsx';
+import SlideShow from './components/Banner.jsx';
 
 function App() {
   return (
     <>
     <Announcement />
     <Menubar />
+    <SlideShow />
     </>
   );
 }
---
+++ luxtus_v1/luxtus_v1/src/components/Banner.jsx
@@ -0,0 +1,22 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+
+
+function Slideshow() {
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                width: '100%',
+                height: '100%',
+                backgroundColor: 'grey',
+            }}
+        >
+
+        </Box>
+    );
+}
+
+export default Slideshow();
